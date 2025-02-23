25c19ce9f3cb7a744f6dac293317efa71b2cd8e5
KinetekEnergy <transyt8@gmail.com>
2025-02-23 12:07:23 -0800

    epic gaming loader bar + sidebar is bricked

--- scripts/convert_notebooks.py
+++ scripts/convert_notebooks.py
@@ -5,6 +5,7 @@ import os
 import nbformat
 import yaml
 import sys
+from scripts.progress_bar import ProgressBar
 
 # Specify the directory where your Jupyter Notebook files are located
 notebook_directory = "_notebooks"
@@ -16,6 +17,7 @@ def error_cleanup(notebook_file):
     # Remove the destination file if it exists
     destination_file = os.path.basename(notebook_file).replace(".ipynb", "_IPYNB_2_.md")
     destination_path = os.path.join(destination_directory, destination_file)
+    
     if os.path.exists(destination_path):
         os.remove(destination_path)
 
@@ -23,6 +25,7 @@ def extract_front_matter(notebook_file, cell):
     front_matter = {}
     
     source = cell.get('source', '')
+    
     if source.startswith('---'):
         # Extract front matter from source
         try:
@@ -37,16 +40,19 @@ def extract_front_matter(notebook_file, cell):
 def get_relative_output_path(notebook_file):
     # Calculate the relative path of the notebook file
     relative_path = os.path.relpath(notebook_file, notebook_directory)
+    
     # Replace the file extension and prepend the subdirectory structure
     markdown_filename = relative_path.replace(".ipynb", "_IPYNB_2_.md")
+    
     # Construct the full path for the output file
     destination_path = os.path.join(destination_directory, markdown_filename)
+    
     return destination_path
 
 def ensure_directory_exists(path):
     # Ensure the directory of the given path exists
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    
+
 # Function to convert the notebook to Markdown with front matter
 def convert_notebook_to_markdown_with_front_matter(notebook_file):
     # Load the notebook file
@@ -79,7 +85,7 @@ def convert_notebook_to_markdown_with_front_matter(notebook_file):
         # Write the converted Markdown file
         with open(destination_path, "w", encoding="utf-8") as file:
             file.write(markdown_with_front_matter)
-            
+
 # Function to convert the Jupyter Notebook files to Markdown
 def convert_single_notebook(notebook_file):
     try:
@@ -92,13 +98,22 @@ def convert_single_notebook(notebook_file):
 def convert_notebooks():
     # Use recursive glob pattern to find .ipynb files in all subdirectories
     notebook_files = glob.glob(f"{notebook_directory}/**/*.ipynb", recursive=True)
+
+    # create progress bar
+    convertBar = ProgressBar(
+        userInfo="Notebook conversion progress:", total=(len(notebook_files))
+    )
+    
     for notebook_file in notebook_files:
         try:
             convert_single_notebook(notebook_file)
+            convertBar.continue_progress()
         except ConversionException as e:
             print(f"Conversion error for {notebook_file}: {str(e)}")
             error_cleanup(notebook_file)
             sys.exit(1)
+    
+    convertBar.end_progress()
 
 # Call the function to perform conversions when the script is run directly
 if __name__ == "__main__":
