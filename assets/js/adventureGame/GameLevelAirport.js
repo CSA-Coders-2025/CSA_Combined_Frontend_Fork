21f688f8144689e37c9c5a7d953ba58045a83060
Saaras859 <kodalisaaras@gmail.com>
2025-05-22 12:22:25 -0700

    sigma update

--- assets/js/adventureGame/GameLevelAirport.js
+++ assets/js/adventureGame/GameLevelAirport.js
@@ -722,7 +722,7 @@ class GameLevelAirport {
               "Janet Yellen",
               "Bank Analytics provides a detailed overview of your spending, investments, and savings.\nWould you like to proceed to the analytics dashboard?",
               [
-                { label: "Open Analytics", action: () => showYellenModal("https://nighthawkcoders.github.io/portfolio_2025/bank/analytics") },
+                { label: "Open Analytics", action: () => showYellenModal("https://nighthawkcoders.github.io/portfolio_2025/gamify/bankanalytics") },
                 { label: "Back", action: () => dialogFunctions.intro(), keepOpen: true }
               ]
             );
